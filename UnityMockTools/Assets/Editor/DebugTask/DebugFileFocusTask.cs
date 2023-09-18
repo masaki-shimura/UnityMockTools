e34e4861d2af990bb1e59cfcfb3b99586f98b26d
masaki-shimura <operationsim.game@gmail.com>
2023-09-18 15:16:22 +0900

    Update ScriptableObjectからデータを参照する事で任意のパスを設定できるように修正

--- UnityMockTools/Assets/Editor/DebugTask/DebugFileFocusTask.cs
+++ UnityMockTools/Assets/Editor/DebugTask/DebugFileFocusTask.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace MSLib
 {
@@ -11,12 +10,15 @@ namespace MSLib
     {
         private const string overviewText = "「Project」Windowで任意のファイルをフォーカスする事が出来ます";
         private string[] paths = new[] { "IDebugTask" };//Assets/.../
+        private EditorFilePathsAsset editorFilePathsAsset = null;
+        private const string SettingFilePath = "Assets/Editor/DebugScriptableObject/EditorFilePaths.asset";
         
         public bool IsFoldout { get; set; }
 
         public void Init()
-        {
-            
+        { 
+            editorFilePathsAsset = AssetDatabase.LoadAssetAtPath<EditorFilePathsAsset>(SettingFilePath);
+            Assert.IsNotNull(editorFilePathsAsset,"設定ファイルがNullでした");
         }
 
         public void UnInit()
@@ -31,9 +33,28 @@ namespace MSLib
 
         public void Draw()
         {
-            if (GUILayout.Button("Focus"))
+            using (new GUILayout.VerticalScope())
             {
-                Execution();
+                using (new GUILayout.HorizontalScope())
+                {
+                    EditorGUILayout.LabelField("ラベル",GUILayout.MaxWidth(150.0f));
+                    EditorGUILayout.LabelField("ファイルパス",GUILayout.MaxWidth(500.0f));
+                    EditorGUILayout.LabelField("",GUILayout.MaxWidth(100.0f));
+                }
+                
+                foreach (var pathInfo in editorFilePathsAsset.PathList)
+                {
+                    var path = pathInfo.path;
+                    using (new GUILayout.HorizontalScope())
+                    {
+                        EditorGUILayout.LabelField(pathInfo.label,GUILayout.MaxWidth(150.0f));
+                        EditorGUILayout.LabelField(path,GUILayout.MaxWidth(500.0f));
+                        if (GUILayout.Button("Focus",GUILayout.MaxWidth(100.0f)))
+                        {
+                            Execution(path);
+                        }
+                    }
+                }
             }
         }
 
@@ -50,6 +71,18 @@ namespace MSLib
         {
             string path = "Assets/Editor";
             
+            var obj = AssetDatabase.LoadAssetAtPath(path, typeof(UnityEngine.Object)); 
+            EditorUtility.FocusProjectWindow();
+            
+            var projectBrowserType = Type.GetType("UnityEditor.ProjectBrowser,UnityEditor");
+            var instance = projectBrowserType.GetField("s_LastInteractedProjectBrowser", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public).GetValue(null);
+            
+            var methodInfo = projectBrowserType.GetMethod("ShowFolderContents", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            methodInfo.Invoke(instance, new object[] { obj.GetInstanceID(), true });
+        }
+        
+        public void Execution(string path)
+        {
             var obj = AssetDatabase.LoadAssetAtPath(path, typeof(UnityEngine.Object)); 
             EditorUtility.FocusProjectWindow();
             
