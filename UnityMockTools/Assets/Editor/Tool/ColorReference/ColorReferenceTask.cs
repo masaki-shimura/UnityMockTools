4cfe931512ed4d95f2a3df02be9f05851d2632de
masaki-shimura <operationsim.game@gmail.com>
2024-04-26 08:57:34 +0900

    Add カラーリファレンス情報を保持し設定するクラスの追加

---
+++ UnityMockTools/Assets/Editor/Tool/ColorReference/ColorReferenceTask.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using UnityEditor;
+
+namespace MSLib
+{
+    public sealed class ColorReferenceTask : IDebugTask
+    {
+        public bool IsFoldout { get; set; }
+        private const string OverviewText = "リファレンス用のカラー情報を保持します";
+        private const string HeaderText = "リファレンス カラー";
+
+        private static List<ColorReference> _colorReferences = new List<ColorReference>(5)
+        {
+            new(Color.White, "初期カラー1"),
+            new(Color.Aqua, "初期カラー2"),
+            new(Color.Aquamarine, "初期カラー3"),
+        };
+
+        public void Init()
+        {
+        }
+
+        public void UnInit()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw()
+        {
+            EditorGUILayout.LabelField(HeaderText);
+        }
+
+        public void Execution()
+        {
+        }
+
+        public string GetOverviewText()
+        {
+            return OverviewText;
+        }
+    }
+}
