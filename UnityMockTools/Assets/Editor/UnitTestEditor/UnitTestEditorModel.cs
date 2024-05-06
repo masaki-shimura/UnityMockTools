20604f395d29b75756b74ba3609811b1502d1855
masaki-shimura <operationsim.game@gmail.com>
2024-05-06 12:55:36 +0900

    Add モデルクラスにテストの実行用処理の追加

--- UnityMockTools/Assets/Editor/UnitTestEditor/UnitTestEditorModel.cs
+++ UnityMockTools/Assets/Editor/UnitTestEditor/UnitTestEditorModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using NUnit.Framework;
+using UnityEditor;
+using UnityEditor.TestTools.TestRunner.Api;
+using UnityEngine;
 
 namespace MSLib
 {
@@ -11,10 +14,15 @@ namespace MSLib
         private readonly string[] _categoryNameList = null;
         public IReadOnlyCollection<string> CategoryNameList => _categoryNameList;
 
+        private TestRunnerApi _testRunnerApi = null;
+
         public UnitTestEditorModel()
         {
             _categoryNameList = GetAllTestCategoryName();
             Assert.IsNotNull(_categoryNameList, "カテゴリ属性が取得出来ませんでした。NUnitでカテゴリ属性が使われていない可能性があります。");
+            _testRunnerApi = ScriptableObject.CreateInstance<TestRunnerApi>();
+            _testRunnerApi.RegisterCallbacks(new UnitTestEditorTestExecutionCallback(
+                (testName) => { EditorUtility.DisplayDialog("テスト結果", $"テストに失敗しました。:{testName}", "ok"); }));
         }
 
         /// <summary>
@@ -54,6 +62,30 @@ namespace MSLib
                     .Select(attribute => attribute.Name));
 
             return categoryNameList;
+        }
+
+        public void RunEditTest()
+        {
+            var filter = new Filter
+            {
+                testMode = TestMode.EditMode,
+                testNames = null,
+                groupNames = null,
+                categoryNames = null,
+                assemblyNames = null,
+                targetPlatform = null,
+            };
+
+            _testRunnerApi.Execute(new ExecutionSettings(filter));
+        }
+
+        public void RunEditTest(string[] categoryNameList)
+        {
+            _testRunnerApi.Execute(new ExecutionSettings(new Filter()
+            {
+                testMode = TestMode.EditMode,
+                categoryNames = categoryNameList
+            }));
         }
     }
 }
