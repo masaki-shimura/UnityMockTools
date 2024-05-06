d86f084be65d2348a579c059cda6bd8ff0f7d806
masaki-shimura <operationsim.game@gmail.com>
2024-05-06 10:47:23 +0900

    Add 属性抽出の為の検証用のテストコードの追加

---
+++ UnityMockTools/Assets/Editor/Tests/UnitTestEditorWindowTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace MSLib
+{
+    [TestFixture]
+    public class UnitTestEditorWindowTest
+    {
+        [Test, Category("CategoryTestA")]
+        public void CategoryTestA()
+        {
+            Debug.Log("カテゴリを定義したサンプルメソッド の実行をしました");
+        }
+
+        [Test, Category("CategoryTestB")]
+        public void CategoryTestB()
+        {
+            Debug.Log("カテゴリを定義したサンプルメソッド の実行をしました");
+        }
+    }
+}
