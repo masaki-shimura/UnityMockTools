fde2a5e7f57ca0a791e87dc5ab20a140e1791dc9
masaki-shimura <operationsim.game@gmail.com>
2024-05-06 12:54:29 +0900

    Add エラー時の挙動確認用のテストコードの追加

--- UnityMockTools/Assets/Editor/Tests/UnitTestEditorWindowTest.cs
+++ UnityMockTools/Assets/Editor/Tests/UnitTestEditorWindowTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using UnityEngine;
 
 namespace MSLib
@@ -16,6 +17,13 @@ namespace MSLib
         public void CategoryTestB()
         {
             Debug.Log("カテゴリを定義したサンプルメソッド の実行をしました");
+        }
+
+        [Test, Category("CategoryTestB"), Ignore("エラー実行確認用")]
+        public void ExceptionTest()
+        {
+            Debug.LogError("ExceptionTestの実行をしました");
+            throw new Exception("強制的にエラーを出しました");
         }
     }
 }
