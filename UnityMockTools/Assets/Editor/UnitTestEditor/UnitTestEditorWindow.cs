31e63d4f6a030f4a777b67e358ad7fa1db4391b2
masaki-shimura <operationsim.game@gmail.com>
2024-05-06 10:48:11 +0900

    Add 単体テストを独自実行する為のエディタウィンドウの追加

---
+++ UnityMockTools/Assets/Editor/UnitTestEditor/UnitTestEditorWindow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+
+namespace MSLib
+{
+    public sealed class UnitTestEditorWindow : EditorWindow
+    {
+        private UnitTestEditorModel _unitTestEditorModel = null;
+
+        [MenuItem("Tools/UnitTest/DebugUnitTestEditorWindow")]
+        static void Initialize()
+        {
+            var window =
+                (UnitTestEditorWindow)EditorWindow.GetWindow(typeof(UnitTestEditorWindow));
+            window.Show();
+        }
+
+        UnitTestEditorWindow()
+        {
+            _unitTestEditorModel = new UnitTestEditorModel();
+        }
+
+        private void OnGUI()
+        {
+            if (_unitTestEditorModel.CategoryNameList.Count <= 0)
+            {
+                EditorGUILayout.LabelField("表示するカテゴリがありません");
+                return;
+            }
+
+            EditorGUILayout.LabelField($"カテゴリ情報");
+            foreach (var info in _unitTestEditorModel.CategoryNameList)
+            {
+                EditorGUILayout.LabelField($"{info}");
+            }
+        }
+    }
+}
