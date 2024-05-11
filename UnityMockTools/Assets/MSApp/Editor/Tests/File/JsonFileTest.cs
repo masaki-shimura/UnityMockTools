f9914a815f7b297dd505de2618208dc64bdbd9e3
masaki-shimura <operationsim.game@gmail.com>
2024-05-12 08:06:55 +0900

    Add テストコードの追加

---
+++ UnityMockTools/Assets/MSApp/Editor/Tests/File/JsonFileTest.cs
@@ -0,0 +1,47 @@
+using System;
+using MSApp.Scripts.File;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace MSApp.Editor.Tests.File
+{
+    public class JsonFileTest
+    {
+        [Test]
+        public void ConvertToJsonTest()
+        {
+            var jsonFile = new JsonFile<DummyData>();
+            var dummyData = new DummyData("DummyTextです", -1);
+
+            var jsonText = jsonFile.ConvertToJson(dummyData);
+
+            Debug.Log($"jsonText:{jsonText}");
+            Assert.IsNotNull(jsonText);
+            Assert.IsNotEmpty(jsonText);
+        }
+
+        [Test]
+        public void LoadObjectTest()
+        {
+            var jsonFile = new JsonFile<DummyData>();
+
+            var inputText = "{\"dummyText\":\"Dummy情報です\",\"dummyIndex\":-1}";
+            var dummyData = jsonFile.LoadObject(inputText);
+
+            Debug.Log($"text:{dummyData.dummyText} / index:{dummyData.dummyIndex}");
+        }
+    }
+
+    [Serializable]
+    public class DummyData
+    {
+        public string dummyText = "Dummy情報です";
+        public int dummyIndex = 1;
+
+        public DummyData(string dummyText, int dummyIndex)
+        {
+            this.dummyIndex = dummyIndex;
+            this.dummyIndex = dummyIndex;
+        }
+    }
+}
