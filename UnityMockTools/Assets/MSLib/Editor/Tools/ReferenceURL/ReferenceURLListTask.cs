fe60764640073280ecd47e04a24a878a4fa65e61
masaki-shimura <operationsim.game@gmail.com>
2024-05-12 14:01:12 +0900

    Add URL資料の表示を管理するクラスの追加

---
+++ UnityMockTools/Assets/MSLib/Editor/Tools/ReferenceURL/ReferenceURLListTask.cs
@@ -0,0 +1,32 @@
+﻿namespace MSLib.Editor.Tools.ReferenceURL
+{
+    public class ReferenceURLListTask : IEditorWindowTask
+    {
+        public bool IsFoldout { get; set; }
+
+        public void Init()
+        {
+        }
+
+        public void UnInit()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw()
+        {
+        }
+
+        public void Execution()
+        {
+        }
+
+        public string GetOverviewText()
+        {
+            return this.GetType().Name;
+        }
+    }
+}
