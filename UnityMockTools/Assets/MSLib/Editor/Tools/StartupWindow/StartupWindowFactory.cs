08d8fa1afdf1b38fe4e06f7c1e98af86202b83f3
masaki-shimura <operationsim.game@gmail.com>
2024-05-25 09:17:23 +0900

    Add スタートアップウィンドウをUnityEditor時に開くようにイベントの紐づけ

---
+++ UnityMockTools/Assets/MSLib/Editor/Tools/StartupWindow/StartupWindowFactory.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+namespace MSLib.Editor.Tools.StartupWindow
+{
+    /// <summary>
+    /// スタートアップウィンドウをUnityEditor上で起動した直後に起動出来るようにイベントの登録を行なう
+    /// </summary>
+    [InitializeOnLoad]
+    public sealed class StartupWindowFactory
+    {
+        static StartupWindowFactory()
+        {
+            EditorApplication.update += Startup;
+        }
+
+        private static void Startup()
+        {
+            StartupWindow.ShowWindow();
+            EditorApplication.update -= Startup;
+        }
+    }
+}
