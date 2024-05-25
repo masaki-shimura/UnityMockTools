cda8065286d56aae5f904654279572a0c165134c
masaki-shimura <operationsim.game@gmail.com>
2024-05-25 10:52:55 +0900

    Update 設定ファイルではなくハードコーディングする形で修正

--- UnityMockTools/Assets/MSLib/Editor/Tools/StartupWindow/StartupWindow.cs
+++ UnityMockTools/Assets/MSLib/Editor/Tools/StartupWindow/StartupWindow.cs
@@ -6,31 +6,19 @@ namespace MSLib.Editor.Tools.StartupWindow
 {
     public sealed class StartupWindow : EditorWindow
     {
-        private static StartupWindowSetting _settingAsset = null;
-
         [MenuItem("Tools/Startup Window")]
         public static void ShowWindow()
         {
             GetWindow<StartupWindow>("Startup Window");
-            _settingAsset = StartupWindowSetting.LoadAsset();
         }
 
         private void OnGUI()
         {
-            _settingAsset =
-                EditorGUILayout.ObjectField(_settingAsset, typeof(StartupWindowSetting)) as
-                    StartupWindowSetting;
-            if (_settingAsset == null)
-            {
-                EditorGUILayout.LabelField("「StartupWindowSetting」の設定ファイルが参照出来ていません");
-                return;
-            }
-
             EditorGUILayout.LabelField($"{DateTime.Now:yyyy/MM/dd HH:mm}");
-            EditorGUILayout.LabelField(_settingAsset.titleText);
+            EditorGUILayout.LabelField("■ スタートアップ画面");
             using (new GUILayout.VerticalScope(GUI.skin.box))
             {
-                EditorGUILayout.LabelField(_settingAsset.bodyText);
+                EditorGUILayout.LabelField("ヽ(^o^)丿＜ 今日も頑張っていきましょう");
             }
         }
     }
