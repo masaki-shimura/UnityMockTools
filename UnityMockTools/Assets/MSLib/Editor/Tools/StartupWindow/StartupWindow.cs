5644ad19ec97559dd71d06c464bd110755de7f47
masaki-shimura <operationsim.game@gmail.com>
2024-05-25 10:14:54 +0900

    Update 表示物を設定ファイルから参照するように修正

--- UnityMockTools/Assets/MSLib/Editor/Tools/StartupWindow/StartupWindow.cs
+++ UnityMockTools/Assets/MSLib/Editor/Tools/StartupWindow/StartupWindow.cs
@@ -1,19 +1,37 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 namespace MSLib.Editor.Tools.StartupWindow
 {
     public sealed class StartupWindow : EditorWindow
     {
+        private static StartupWindowSetting _settingAsset = null;
+
         [MenuItem("Tools/Startup Window")]
         public static void ShowWindow()
         {
             GetWindow<StartupWindow>("Startup Window");
+            _settingAsset = StartupWindowSetting.LoadAsset();
         }
 
         private void OnGUI()
         {
-            GUILayout.Label("Startup Window");
+            _settingAsset =
+                EditorGUILayout.ObjectField(_settingAsset, typeof(StartupWindowSetting)) as
+                    StartupWindowSetting;
+            if (_settingAsset == null)
+            {
+                EditorGUILayout.LabelField("「StartupWindowSetting」の設定ファイルが参照出来ていません");
+                return;
+            }
+
+            EditorGUILayout.LabelField($"{DateTime.Now:yyyy/MM/dd HH:mm}");
+            EditorGUILayout.LabelField(_settingAsset.titleText);
+            using (new GUILayout.VerticalScope(GUI.skin.box))
+            {
+                EditorGUILayout.LabelField(_settingAsset.bodyText);
+            }
         }
     }
 }
