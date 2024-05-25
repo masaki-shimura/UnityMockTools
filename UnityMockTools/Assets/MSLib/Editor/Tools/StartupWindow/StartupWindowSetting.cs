dbfad7a714ecd06494d4fe758e970f87e4a9cc01
masaki-shimura <operationsim.game@gmail.com>
2024-05-25 10:13:33 +0900

    Add 設定ファイル用の「ScriptableObject」クラスの追加

---
+++ UnityMockTools/Assets/MSLib/Editor/Tools/StartupWindow/StartupWindowSetting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace MSLib.Editor.Tools.StartupWindow
+{
+    [CreateAssetMenu(fileName = "StartupWindowSetting",
+        menuName = "ScriptableObjects/StartupWindowSetting")]
+    public sealed class StartupWindowSetting : ScriptableObject
+    {
+        public string titleText = "";
+        public string bodyText = "";
+        private static string _assetPath = "";
+        private const string DefaultAssetPath = "Assets/MSLib/Editor/Tools/StartupWindow/StartupWindowSetting.asset";
+
+        private StartupWindowSetting()
+        {
+            _assetPath = DefaultAssetPath;
+        }
+
+        public static StartupWindowSetting LoadAsset(string path = null)
+        {
+            if (String.IsNullOrEmpty(path) == false)
+            {
+                _assetPath = path;
+            }
+
+            var asset = AssetDatabase.LoadAssetAtPath<StartupWindowSetting>(_assetPath);
+            Assert.IsNotNull(asset, "設定ファイルがNullでした");
+            return asset;
+        }
+    }
+}
