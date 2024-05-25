c1df681691ac20712d6b2bdbf4c32747db914691
masaki-shimura <operationsim.game@gmail.com>
2024-05-25 09:16:48 +0900

    Add Startupウィンドウの追加

---
+++ UnityMockTools/Assets/MSLib/Editor/Tools/StartupWindow/StartupWindow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MSLib.Editor.Tools.StartupWindow
+{
+    public sealed class StartupWindow : EditorWindow
+    {
+        [MenuItem("Tools/Startup Window")]
+        public static void ShowWindow()
+        {
+            GetWindow<StartupWindow>("Startup Window");
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Startup Window");
+        }
+    }
+}
